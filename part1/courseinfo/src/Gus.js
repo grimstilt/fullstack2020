be6a63efbf67bd70f64c24b87b7207ae3c24aa81
grimstilt <kraunchapakshi@gmail.com>
2021-07-12 13:00:57 -0500

    updated

---
+++ part1/courseinfo/src/Gus.js
@@ -0,0 +1,22 @@
+const Object1 = {
+    name: 'Gus Bus',
+    age: '10',
+    education: 'lounging',
+}
+
+const Object2 = {
+    name: 'Full Stack',
+    level: 'Intermediate',
+    size: 5,
+}
+
+const Object3 = {
+    name: {
+        first: 'Gus',
+        Last: 'Bus',
+    },
+    grades: [10, 10, 10],
+    department: 'Eating'
+}
+
+console.log(Object1.name);
